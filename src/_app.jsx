1d284887b2877c5b528aca6e198ad82f113e829a
Andrij Kalko <andrey.kallko@gmail.com>
2023-01-17 20:00:55 +0200

    hashrouter

--- src/_app.jsx
+++ src/_app.jsx
@@ -1,17 +1,17 @@
-import React from "react"
-import { RecoilRoot } from 'recoil'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import { HelmetProvider } from 'react-helmet-async'
-import { ROUTE_NAMES } from './data/data'
+import React from 'react';
+import { RecoilRoot } from 'recoil';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
+import { ROUTE_NAMES } from './data/data';
 
-import Header from './components/Header'
-import HomePage from './pages/HomePage'
-import AboutUsPage from './pages/AboutUsPage'
-import ServicesPage from './pages/ServicesPage'
-import ServicePage from './pages/ServicePage'
-import FeedbackPage from './pages/FeedbackPage'
-import ScrollToTop from './components/ScrollToTop'
-import Footer from './components/Footer'
+import Header from './components/Header';
+import HomePage from './pages/HomePage';
+import AboutUsPage from './pages/AboutUsPage';
+import ServicesPage from './pages/ServicesPage';
+import ServicePage from './pages/ServicePage';
+import FeedbackPage from './pages/FeedbackPage';
+import ScrollToTop from './components/ScrollToTop';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -22,7 +22,11 @@ function App() {
           <Routes>
             <Route path={ROUTE_NAMES.HOME} element={<HomePage />} />
             <Route path={ROUTE_NAMES.ABOUT_US} element={<AboutUsPage />} />
-            <Route path={ROUTE_NAMES.SERVISES} element={<ServicesPage />} exact />
+            <Route
+              path={ROUTE_NAMES.SERVISES}
+              element={<ServicesPage />}
+              exact
+            />
             <Route path={ROUTE_NAMES.SERVISE} element={<ServicePage />} />
             <Route path={ROUTE_NAMES.FEEDBACK} element={<FeedbackPage />} />
           </Routes>
@@ -31,7 +35,7 @@ function App() {
         </Router>
       </RecoilRoot>
     </HelmetProvider>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './styles/index.scss'
+import './styles/index.scss';
 import reportWebVitals from './reportWebVitals';
 
 import App from './_app';
